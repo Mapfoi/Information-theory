6990aa55457e30c1964c48da789069dfcad7a1aa
Mapfoi <seva.eshmanskii1109@gmail.com>
2026-03-01 21:24:35 +0300

    correct version of exe file

--- Lab1 The simplest ciphers/Source code/Lab1/obj/Debug/net10.0-windows/Lab1.AssemblyInfo.cs
+++ Lab1 The simplest ciphers/Source code/Lab1/obj/Debug/net10.0-windows/Lab1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64bf72e4a833dde205ee8075242b530f72dd8410")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82a9096dbafab15961f5ebd5c2fd20954d44bd42")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
